78dd918408596141855774e24dc8da2c4e6e1a0b
Vegh Adam <veghadam1991@gmail.com>
2016-05-03 04:47:26 +0200

    added exercise 2: use a mutex

---
+++ SingleInstance/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SingleInstance
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Mutex myMutex = null;
+			const string MutexName = "RUNMEONCE";
+
+			try
+			{
+				myMutex = Mutex.OpenExisting(MutexName);
+			}
+			catch (WaitHandleCannotBeOpenedException)
+			{
+				Console.WriteLine("Sorry, but the mutex called {0} doesn't exist...", MutexName);
+				Console.WriteLine("Press enter to make one...");
+				Console.ReadLine();
+			}
+
+			if (myMutex == null)
+				myMutex = new Mutex(true, MutexName);
+			else
+			{
+				myMutex.Close();
+				return;
+			}
+
+			Console.WriteLine("Yesss, I've done it!\nPress enter to exit...");
+			Console.ReadLine();
+		}
+	}
+}
