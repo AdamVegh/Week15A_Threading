4780afeca50fd66da2cefb731a0fa0243b310391
Vegh Adam <veghadam1991@gmail.com>
2016-05-03 05:01:49 +0200

    added exercise 3: use threadpool

---
+++ ThreadPoolDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadPoolDemo
+{
+	class Program
+	{
+		static void ShowMyText(object state)
+		{
+			string myText = (string)state;
+			Console.WriteLine("Thread ID: {0}, Text: {1}", Thread.CurrentThread.ManagedThreadId, myText);
+		}
+
+		static void Main(string[] args)
+		{
+			WaitCallback wcb = new WaitCallback(ShowMyText);
+
+			ThreadPool.QueueUserWorkItem(wcb, "Python");
+			ThreadPool.QueueUserWorkItem(wcb, "MySQL");
+			ThreadPool.QueueUserWorkItem(wcb, "Java");
+			ThreadPool.QueueUserWorkItem(wcb, "CSharp");
+
+			Console.WriteLine("Press enter to exit...");
+			Console.ReadLine();
+		}
+	}
+}
